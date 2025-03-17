462dad5bbdec15bec52aa3bb01c23e068b49773c
parkjungkwan <pakjkwan@gmail.com>
2025-03-17 14:35:03 +0900

    .

--- com/epislab/account/guest/customer/services/create_customer_service.py
+++ com/epislab/account/guest/customer/services/create_customer_service.py
@@ -1,12 +1,22 @@
 from com.epislab.account.guest.customer.models.customer_schema import CustomerSchema
 from sqlalchemy.ext.asyncio import AsyncSession
-from com.epislab.account.guest.customer.storages.create_customer import DefaultCreateRepository
+from com.epislab.account.guest.customer.storages import create_customer
 from com.epislab.utils.creational.abstract.abstract_service import AbstractService
 
 
 class CustomerCreate(AbstractService):
 
-    async def handle(self, db: AsyncSession, new_customer: CustomerSchema):
-        customer_repo = DefaultCreateRepository()
-        return await customer_repo.create(db, new_customer)
+    async def handle(self, **kwargs):
+        db: AsyncSession = kwargs.get("db")
+        schema: CustomerSchema = kwargs.get("customer")
+        try:
+            customer = await create_customer(db, schema)
+            db.add(customer)
+            await db.commit()
+            await db.refresh(customer)
+            return customer
+        except Exception as e:
+            print(f"[ERROR] CustomerCreate failed: {e}")
+            await db.rollback()
+            raise e
 
--- com/epislab/account/guest/customer/storages/create_customer.py
+++ com/epislab/account/guest/customer/storages/create_customer.py
@@ -1,24 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from com.epislab.account.guest.customer.models.customer_entity import CustomerEntity
 from com.epislab.account.guest.customer.models.customer_schema import CustomerSchema
-from com.epislab.account.guest.customer.service.create_service import CreateService
 
 
-class DefaultCreateRepository(CreateService):
+async def create_customer(db: AsyncSession, new_customer: CustomerSchema):
+    db.add(CustomerEntity(
+        user_id = new_customer.user_id,
+        name = new_customer.name,
+        email = new_customer.email,
+        password = new_customer.password
+    ))
 
-    async def create(self, new_customer: CustomerSchema, db: AsyncSession):
-        print("🚀🤖Repository new_customer 정보 : ", new_customer)
-        db.add(CustomerEntity(
-            user_id = new_customer.user_id,
-            name = new_customer.name,
-            email = new_customer.email,
-            password = new_customer.password
-        ))
-        db.commit()
-        db.refresh(new_customer)
-        return new_customer
 
-class ValidatedCreateRepository(CreateService):
-
-    async def create(self, db: AsyncSession, new_customer: CustomerSchema):
-        pass
