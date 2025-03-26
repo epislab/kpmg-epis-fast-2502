ac072f8f7b7d83f15186bb954e8e713aed9f12f2
parkjungkwan <pakjkwan@gmail.com>
2025-03-26 17:29:52 +0900

    .

--- com/epislab/account/auth/user/services/user_lookup.py
+++ com/epislab/account/auth/user/services/user_lookup.py
@@ -14,35 +14,35 @@ class Login(AbstractService):
         # user_schema는 dict 또는 Pydantic 모델
         user_dict = user_schema if isinstance(user_schema, dict) else user_schema.dict()
 
-        user_id = user_dict.get("user_id")
+        email = user_dict.get("email")
         password = user_dict.get("password")
 
         # 1단계: user_id 존재 여부 확인
-        check_stmt, check_params = get_check_user_id_stmt(user_id)
+        check_stmt, check_params = get_check_user_id_stmt(email)
         check_result: Result = await db.execute(check_stmt, check_params)
 
         if check_result.fetchone() is None:
             return {
-                "message": "고객에서 등록된 ID가 없습니다",
-                "data": {}
+                "message": "고객에서 등록된 Email 이이 없습니다",
+                "logged_in_user": {}
             }
 
         # 2단계: user_id + password 검사
-        login_stmt, login_params = get_login_stmt(user_id, password)
+        login_stmt, login_params = get_login_stmt(email, password)
         login_result: Result = await db.execute(login_stmt, login_params)
 
-        row = login_result.fetchone()
+        logged_in_user = login_result.fetchone()
 
-        if row is None:
+        if logged_in_user is None:
             return {
                 "message": "비밀번호가 일치하지 않습니다",
-                "data": {}
+                "logged_in_user": {}
             }
 
         # 3단계: 로그인 성공
         return {
             "message": "로그인에 성공했습니다",
-            "data": dict(row._mapping)
+            "logged_in_user": dict(logged_in_user._mapping)
         }
 
 
--- com/epislab/account/auth/user/sql/dql.sql
+++ com/epislab/account/auth/user/sql/dql.sql
@@ -2,3 +2,10 @@ SELECT * FROM member;
 
 
 SELECT user_id, email, name FROM member;
+
+
+SELECT *
+FROM member
+where user_id = 'kim' and password = '1234'
+;
+
